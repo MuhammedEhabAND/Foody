66d8a8b10f5272f5ffc826e8b67adba848298ed6
MuhammedEhabAND <muhammedehab726@gmail.com>
2023-09-29 23:26:01 +0300

    adding fav fragment , search fragment , profile fragment and country search fragment set it right with the bottom navigation bar to switch between them easily

--- app/src/main/java/inc/moe/foody/HomeActivity.java
+++ app/src/main/java/inc/moe/foody/HomeActivity.java
@@ -1,14 +1,31 @@
 package inc.moe.foody;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class HomeActivity extends AppCompatActivity {
-
-    @Override
+     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+         BottomNavigationView bottomNavigationView = findViewById(R.id.navigator_bar);
+         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+         NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+     }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/inc/moe/foody/country_feature/view/CountryFragment.java
@@ -0,0 +1,30 @@
+package inc.moe.foody.country_feature.view;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import inc.moe.foody.R;
+
+public class CountryFragment extends Fragment {
+
+    public CountryFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_country, container, false);
+    }
+}
