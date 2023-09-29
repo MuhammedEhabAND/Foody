c2c8615d346838a121b322446e0be326d35a2ff9
MuhammedEhabAND <muhammedehab726@gmail.com>
2023-09-29 03:48:24 +0300

    adding Repo and its interface to fetch all categories

---
+++ app/src/main/java/inc/moe/foody/model/IRepo.java
@@ -0,0 +1,9 @@
+package inc.moe.foody.model;
+
+import inc.moe.foody.network.MyNetworkCallBack;
+
+public interface IRepo {
+
+    void makeNetworkCall(MyNetworkCallBack myNetworkCallBack);
+
+}
--- app/src/main/java/inc/moe/foody/model/Repo.java
+++ app/src/main/java/inc/moe/foody/model/Repo.java
@@ -1,4 +1,27 @@
 package inc.moe.foody.model;
 
-public class Repo {
+import inc.moe.foody.network.MyNetworkCallBack;
+import inc.moe.foody.network.RemoteSource;
+
+public class Repo implements IRepo{
+    RemoteSource remoteSource;
+    private static Repo instance = null;
+
+
+
+    private Repo(RemoteSource remoteSource) {
+        this.remoteSource = remoteSource;
+
+    }
+
+    public static Repo getInstance(RemoteSource remoteSource) {
+        if (instance == null) return new Repo(remoteSource);
+        return instance;
+    }
+
+
+    @Override
+    public void makeNetworkCall(MyNetworkCallBack myNetworkCallBack) {
+        remoteSource.makeNetworkCallForCategories(myNetworkCallBack);
+    }
 }
